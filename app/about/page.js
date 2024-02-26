43d802c8635659bd4d6b615fb28cb0f5120d51a1
Haya AlMajali <hayamaj@crc-dot1x-nat-10-239-245-3.bu.edu>
2024-02-26 09:44:03 -0500

    width

--- app/about/page.js
+++ app/about/page.js
@@ -131,13 +131,15 @@ export default function Home() {
         <div className="w-full flex flex-col sm:flex-row"> {/* Use flexbox for layout */}
     {/* Conditionally render the video based on screen size */}
     {window.innerWidth > 640 && (
-      <div className="w-1/2 pr-0 sm:pr-4 mb-4 sm:mb-0"> {/* Adjust width and spacing */}
+          <div className="w-1/2 pr-0 sm:pr-4 mb-4 sm:mb-0"> {/* Adjust width and spacing */}
+
       <video autoPlay loop playsInline muted className="w-full">
           <source src="/aboutvid.mov" />
         </video>
-      </div>
+     </div>
     )}
-    <div className="w-full sm:w-1/2">
+     </div>
+     <div className={`w-full sm:w-${window.innerWidth > 640 ? '1/2' : 'full'}`}> {/* Adjust width */}
     <h1 className="text-lg mb-2">Hi! I'm Haya.</h1>
                         <h1 className={`${dilight.className} text-sm text-black text-justify`}>
                             I am a Jordanian-born student at Boston University, on track to graduate in May 2024.
